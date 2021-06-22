fa09608c1c986ffaef7ca0254752a8c94286945e
Pavan Jain <pavanpeo@gmail.com>
2021-06-22 15:58:07 +0530

    TestCases Added

---
+++ SwiftyLib/SwiftyLib.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftyLib.swift
+//  SwiftyLib
+//
+//  Created by Pavan Jain on 22/06/21.
+//
+
+import Foundation
+
+public final class SwiftyLib {
+     let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+}
